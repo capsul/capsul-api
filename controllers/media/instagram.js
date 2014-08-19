1d020d50183afaeafe79ca6d8e7d263fdffaedfb
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-08-18 21:21:09 -0700

    Restrict InstagramManager's public interface to only one search method.

--- controllers/media/instagram.js
+++ controllers/media/instagram.js
@@ -1,69 +1,72 @@
 module.exports = (function(){
-	return InstagramManager = {
-		collectParams: function(url) {
-			return require("../../helpers")
-			.paramsForUrl(url);
-		},
-		collectOptions: function(params) {
-			return options = {
-				url: 'https://api.instagram.com/v1/media/search?' + 
-				'lat=' + params.lat +
-				'&lng=' + params.lng +
-				'&max_timestamp='+ params.time +
-				'&distance=' + '500' +
-				'&access_token=' + 
-				process.env.INSTAGRAM_ACCESS_TOKEN,
-				headers: { 'User-Agent': 'request' }
-			}
-		},
-		locationForImage: function(image) {
-			return {
-				"latitude": image.location.latitude,
-				"longitude": image.location.longitude
-			}
-		},
-		imagesForImage: function(image) {
-			return {
-				"thumb": image.images.thumbnail.url,
-				"low_res": image.images.low_resolution.url,
-				"high_res": image.images.standard_resolution.url
-			}
-		},
-		captionForImage: function(image) {
-			try {
-				return image.caption.text
-			} catch(e) {
-				return ""
-			}
-		},
-		granularFromImages: function(image) {
-			return granular = {
-				type: 			image.type,
-				created_at: image.created_at,
-				service: 		'instagram',
-				link:       image.link,
-				author:     image.user.username,
-				location:   this.locationForImage(image),
-				images:     this.imagesForImage(image),
-				caption:    this.captionForImage(image),
-				hashtags:   image.tags
-			}
-		},
-		granualsFromInstagramData: function(instagramData) {
-			var granuals = [];
-			instagramData.forEach(function(image) {
-				granuals.push(this.granularFromImages(image));
-			}.bind(this));
-			return granuals;
-		},
-		search: function *(url) {
-			var request = require('koa-request');
+	function collectParamsFromUrl(url) {
+		return require("../../helpers")
+		.paramsForUrl(url);
+	}
 
-			var params = this.collectParams(url)
-			var options = this.collectOptions(params);
-			var response = yield request(options);
+	function collectOptions(params) {
+		return options = {
+			url: 'https://api.instagram.com/v1/media/search?' + 
+			'lat=' + params.lat +
+			'&lng=' + params.lng +
+			'&max_timestamp='+ params.time +
+			'&distance=' + '500' +
+			'&access_token=' + 
+			process.env.INSTAGRAM_ACCESS_TOKEN,
+			headers: { 'User-Agent': 'request' }
+		}
+	}
+
+	function locationForImage(image) {
+		return {
+			"latitude": image.location.latitude,
+			"longitude": image.location.longitude
+		}
+	}
+
+	function imagesForImage(image) {
+		return {
+			"thumb": image.images.thumbnail.url,
+			"low_res": image.images.low_resolution.url,
+			"high_res": image.images.standard_resolution.url
+		}
+	}
+
+	function captionForImage(image) {
+		try {
+			return image.caption.text
+		} catch(e) {
+			return ""
+		}
+	}
+
+	function granularFromImage(image) {
+		return granular = {
+			type: 			image.type,
+			created_at: image.created_at,
+			source: 		'instagram',
+			link:       image.link,
+			author:     image.user.username,
+			location:   locationForImage(image),
+			images:     imagesForImage(image),
+			caption:    captionForImage(image),
+			hashtags:   image.tags
+		}
+	}
+
+	function granualsFromInstagramData(instagramData) {
+		return instagramData.map(function(image) {
+			return granularFromImage(image);
+		});
+	}
+
+	return InstagramManager = {
+		search: function *(url) {
+			var params = collectParamsFromUrl(url)
+			var options = collectOptions(params);
+			var response = yield require('koa-request')(options);
 			var instagramData = JSON.parse(response.body).data
-			return this.granualsFromInstagramData(instagramData)
+			return granualsFromInstagramData(instagramData)
 		}
 	}
 })();
