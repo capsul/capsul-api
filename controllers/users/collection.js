1f2587a913a987fa0eae03bfa4b4dd1cffb8a5e1
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-08-20 21:41:15 -0700

    Refactor to use promises for social media service responses.

--- controllers/users/collection.js
+++ controllers/users/collection.js
@@ -2,18 +2,20 @@ module.exports = (function(){
 
 	// GET /users/:id/media?lat=<LAT>&lng=<LNG>&time=<TIME>
 	return function* collection(id) {
-
-		// Twitter Requests
-    var twitterDef = require('q').defer()
+    var Q = require('q');
     var TwitterManager = require('../media/twitter');
-    var twitterGranuals = twitterDef.promise.then(TwitterManager.search);
-  //   var twitterDef = require('q').defer()
-
-    // var instagramDef = require('q').defer()
     var InstagramManager = require('../media/instagram');
-    // var instagramGranuals = instagramDef.promise.then(InstagramManager.search);
-    var instagramGranuals = InstagramManager.search(this.request.url);
     
+    // Twitter Requests
+    var twitterDef = Q.defer();
+    var twitterGranuals = twitterDef.promise
+                          .then(TwitterManager.search);
+    
+    // Instagram Requests
+    var instagramDef = Q.defer();
+    var instagramGranuals = instagramDef.promise
+                            .then(InstagramManager.search);
+
     // Flickr Requests
     // var FlickrManager = require('../media/flickr');
     // var flickrGranuals = yield FlickrManager.search(this.request.url);
@@ -27,15 +29,11 @@ module.exports = (function(){
       "data": []
     }
 
-    // def.resolve(this.request.url)
-    // var instaGranuals = def.promise.then(InstagramManager.search);
-    // capsul.data.push(instagramGranuals)
-
     twitterDef.resolve(this.request.url)
-    // instagramDef.resolve(this.request.url)
-    
+    instagramDef.resolve(this.request.url)
+
     capsul.data.push(twitterGranuals);
-    capsul.data.push(instagramGranuals)
+    capsul.data.push(instagramGranuals);
 
 		this.body = yield capsul;
 	}
