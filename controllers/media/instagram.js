b4873dfc0a0cbced4914905fa338d9f401f0a75b
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-08-21 01:07:54 -0700

    Refactor instagram request helper methods and fix file spacing.

--- controllers/media/instagram.js
+++ controllers/media/instagram.js
@@ -1,43 +1,42 @@
 module.exports = (function(){
-	
+
   function collectParams(url) {
-		return require("../../helpers").paramsForUrl(url);
-	}
+    return require("../../helpers").paramsForUrl(url);
+  }
 
-	function constructUrl(params) {
-		return url = 'http://instagram-capsul.herokuapp.com/media?' + 
-			'lat=' + params.lat +
-			'&lng=' + params.lng +
-			'&time='+ params.time +
-			'&access_token=' + 
-			process.env.INSTAGRAM_ACCESS_TOKEN
-	}
+  function constructUrl(params) {
+    return url = 'http://instagram-capsul.herokuapp.com/media?' + 
+      'lat=' + params.lat +
+      '&lng=' + params.lng +
+      '&time='+ params.time +
+      '&access_token=' + 
+      process.env.INSTAGRAM_ACCESS_TOKEN
+  }
 
-  // function spawnParamTimes(params) {
-  //  var offset = 8 * (60 * 60)
-    
- //    var pastParams  = JSON.parse(JSON.stringify(params));
-  //  pastParams.time = (Number(params.time) + Number(offset)).toString();
+  function spawnParamTimes(params) {
+    var offset = 8 * (60 * 60)
 
- //    var futureParams  = JSON.parse(JSON.stringify(params));
- //    futureParams.time = (Number(params.time) + Number(offset)).toString();
+    var pastParams  = JSON.parse(JSON.stringify(params));
+    pastParams.time = (Number(params.time) + Number(offset)).toString();
 
-
-  //  return [pastParams, params, futureParams];
-  // }
+    var futureParams  = JSON.parse(JSON.stringify(params));
+    futureParams.time = (Number(params.time) + Number(offset)).toString();
+    console.log([pastParams, params, futureParams])
+    return [pastParams, params, futureParams];
+  }
 
   function mediaResponse(res) {
     return JSON.parse(res.toString('utf8')).media
   }
 
-	return InstagramManager = {
-		search: function (url) {
+  return InstagramManager = {
+    search: function (url) {
       var qHTTP = require("q-io/http");
-			var params = collectParams(url);
+      var params = collectParams(url);
       var requestUrl = constructUrl(params);
 
       return qHTTP.read(requestUrl)
       .then(mediaResponse, console.error)
     }
-	};
+  };
 })();	
