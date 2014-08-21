3db29a897a7e7d88703609ac3366af9a3db82bce
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-08-20 21:39:44 -0700

    INSTAGRAM REFACTOR to use promises that request data from the Twitter Capsul search.

--- controllers/media/instagram.js
+++ controllers/media/instagram.js
@@ -1,98 +1,43 @@
 module.exports = (function(){
-	function collectParamsFromUrl(url) {
-		return require("../../helpers")
-		.paramsForUrl(url);
+	
+  function collectParams(url) {
+		return require("../../helpers").paramsForUrl(url);
 	}
 
-	function spawnParamTimes(params) {
-		var offset = 8 * (60 * 60)
-		var futureParams = JSON.parse(JSON.stringify(params));
-		futureParams.time = (Number(params.time) + Number(offset)).toString();
-
-		var pastParams = JSON.parse(JSON.stringify(params));
-		pastParams.time = (Number(params.time) + Number(offset)).toString();
-
-		return [pastParams, params, futureParams];
-	}
-
-	function collectOptions(params) {
-		return options = {
-			url: 'https://api.instagram.com/v1/media/search?' + 
+	function constructUrl(params) {
+		return url = 'http://instagram-capsul.herokuapp.com/media?' + 
 			'lat=' + params.lat +
 			'&lng=' + params.lng +
-			'&max_timestamp='+ params.time +
-			'&distance=' + '200' +
+			'&time='+ params.time +
 			'&access_token=' + 
-			process.env.INSTAGRAM_ACCESS_TOKEN,
-			headers: { 'User-Agent': 'request' }
-		}
+			process.env.INSTAGRAM_ACCESS_TOKEN
 	}
 
-	function locationForImage(image) {
-		return {
-			"latitude": image.location.latitude,
-			"longitude": image.location.longitude
-		}
-	}
+  // function spawnParamTimes(params) {
+  //  var offset = 8 * (60 * 60)
+    
+ //    var pastParams  = JSON.parse(JSON.stringify(params));
+  //  pastParams.time = (Number(params.time) + Number(offset)).toString();
 
-	function imagesForImage(image) {
-		if( image.type === 'image' ) {
-			return {
-				"thumb": image.images.thumbnail.url,
-				"low_res": image.images.low_resolution.url,
-				"high_res": image.images.standard_resolution.url
-			}
-		}
-	}
+ //    var futureParams  = JSON.parse(JSON.stringify(params));
+ //    futureParams.time = (Number(params.time) + Number(offset)).toString();
 
-	function captionForImage(image) {
-		try {
-			return image.caption.text
-		} catch(e) {
-			return ""
-		}
-	}
 
-	function granularFromImage(image) {
-		return granular = {
-			type: 			image.type,
-			created_at: image.created_at,
-			source: 		'instagram',
-			link:       image.link,
-			author:     image.user.username,
-			location:   locationForImage(image),
-			images:     imagesForImage(image),
-			caption:    captionForImage(image),
-			hashtags:   image.tags
-		}
-	}
+  //  return [pastParams, params, futureParams];
+  // }
 
-	function granualsFromInstagramData(instagramData) {
-		return instagramData.map(function(image) {
-			return granularFromImage(image);
-		});
-	}
+  function mediaResponse(res) {
+    return JSON.parse(res.toString('utf8')).media
+  }
 
 	return InstagramManager = {
-		search: function *(url) {
-			var params = collectParamsFromUrl(url);
-			var paramsSet = spawnParamTimes(params);
-			
-			var optionsSet = paramsSet.map(function(param) {
-				return collectOptions(param)
-			});
+		search: function (url) {
+      var qHTTP = require("q-io/http");
+			var params = collectParams(url);
+      var requestUrl = constructUrl(params);
 
-			var response = yield require('koa-request')(optionsSet[0]);
-			var response2 = yield require('koa-request')(optionsSet[1]);
-			var response3 = yield require('koa-request')(optionsSet[2]);
-
-			var instagramData = JSON.parse(response.body).data
-			var instagramData2 = JSON.parse(response2.body).data
-			var instagramData3 = JSON.parse(response3.body).data
-			var combinedResponse = instagramData.concat(instagramData2)
-															.concat(instagramData3);
-
-			return granualsFromInstagramData(combinedResponse);
-		}
-	}
+      return qHTTP.read(requestUrl)
+      .then(mediaResponse, console.error)
+    }
+	};
 })();	
