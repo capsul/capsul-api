f77ef1f2491cdff346ecdf06a148d24cd337f780
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-09-17 20:32:27 -0700

    Remove all deleted/unnecessary routes.

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,17 @@
-module.exports = function(app, route, controller) {
+// API Resource controller referenecs
+var controller = require('../controllers');
+var capsul = controller.capsul;
+var users = controller.users;
+
+module.exports = function(app) {
 	
-	// API Resource controller referenecs
-	var capsul = controller.capsul;
-	var media = controller.media;
-	var users = controller.users;
-
 	// CapsulE
-	app.use(route.get('/', capsul.home));
-
-	// Media Selections
-	app.use(route.get('/images', media.images));
-	app.use(route.get('/text', media.text));
+	app.get('/', capsul.home);
 
 	// User Routes 
-	app.use(route.get('/users/:id', users.user));
-	app.use(route.post('/users', users.create));
-	app.use(route.put('/users/:id', users.update));
-	app.use(route.delete('/users/:id', users.deleteUser));
-	app.use(route.get('/users/:id/media', users.collection));
-
-	// Test Collection
-	app.use(route.get('/users/:id/test', users.collectionTest));
+	app.get('/users/:id', users.user);
+	app.post('/users', users.create);
+	app.put('/users/:id', users.update);
+	app.delete('/users/:id', users.destroy);
+	app.get('/users/:id/media', users.collection);
 }
