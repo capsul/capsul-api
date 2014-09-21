237f93df5c9d0c25d1b79978ca7cbb8b95c49884
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-09-21 16:48:59 -0700

    Create crossDomain module to attemp cors request issues.

--- app.js
+++ app.js
@@ -5,24 +5,25 @@ var express   = require('express'),
 		async     = require('async'),
 		request   = require('request'),
 		config    = require('./config'),
-		app 		  = express();
+		app 		  = express()
 
 // Including CORS for cross-origin request access
-app.use(cors());
+app.use(cors())
+app.use(config.crossDomain)
 
 // Setting application port
-app.set('port', config.server.port);
+app.set('port', config.server.port)
 
 // Load local environment variables
-dotenv.load();
+dotenv.load()
 
 // Include Capsul API Routes
-require('./routes')(app);
+require('./routes')(app)
 
 // Listen on local/heroku server port
 app.listen(config.server.port, function() {
 	var status = 
 	"Express server listening on port " + app.get('port') + 
-  " in " + process.env.NODE_ENV + " environment " + "...";
-  console.log(status);
-});
+  " in " + process.env.NODE_ENV + " environment " + "..."
+  console.log(status)
+})
--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   server: require('./server'),
+  crossDomain: require('./crossDomain')
   // database: require('./database')
 };
